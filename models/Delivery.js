9725aa9cc08c67360fb2f15ba179b83132d7dab2
Mahora Grolleau <hiphopfraise@gmail.com>
2025-01-30 17:10:02 +0100

    AAAAAAAAAAAAH

--- models/Delivery.js
+++ models/Delivery.js
@@ -1,5 +1,6 @@
 import { Sequelize, DataTypes, Model } from 'sequelize';
-import {User} from './User.js'import { sequelize } from '../core/connexion_database.js';
+import {User} from './User.js';
+import { sequelize } from '../core/connexion_database.js';
 
 export class Delivery extends Model {
   id_deliveror;
