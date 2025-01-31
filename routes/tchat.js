322e862474d2146569a3e1adf4335186aef78eba
Mahora Grolleau <hiphopfraise@gmail.com>
2025-01-31 11:02:19 +0100

    Error tchat

--- routes/tchat.js
+++ routes/tchat.js
@@ -3,9 +3,9 @@ import { base_url } from '../bin/www';
 export let router = Router();
 
 router.post('/', function(req, res, next) {
-    res.render('layout', { page:'dab',title: 'Tchat', logged_user:req.body.pseudo, base_url  });
+    res.render('layout', { page:'dab',title: 'Tchat', isLogged:req.session.logged_in, logged_user:req.body.pseudo, base_url  });
 })
 
 router.get('/login', function(req, res, next) {
-    res.render('layout', { page:'login_tchat',title: 'Connexion au Tchat', isAdmin:false, base_url  });
+    res.render('layout', { page:'login_tchat',title: 'Connexion au Tchat', isLogged:req.session.logged_in, base_url  });
 });
